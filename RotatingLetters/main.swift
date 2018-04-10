435e537c936614c8cfe0ca344e920f7a09f6c0ba
Yuen, Geoff <gyuen@lcs-mba3098.lcs.on.ca>
2018-04-10 14:54:40 -0400

    whoop whoop 1-30 input collection u know whats up my dude

--- RotatingLetters/main.swift
+++ RotatingLetters/main.swift
@@ -11,7 +11,31 @@ import Foundation
 // Get the user input
 var rawInput = readLine()
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+
+// Loop forever until we get good input
+while  1 == 1 {
+    
+    // Print out the input provided
+    print("input word is?")
+    
+    //Wait for input
+    //Make sure it is not nil
+    //Read line() is wait for input
+    
+    guard let givenInput = readLine() else {
+        //If we got here, input is nil
+        // Prompt again
+        print("Please input string between 1-30 UPPERCASE characters with NO SPACES")
+        continue
+    }
+    if givenInput.count < 1 || givenInput.count > 30 || givenInput == " "  {
+            print("Please input string between 1-30 UPPERCASE characters with NO SPACES")
+            continue
+    }
+    
+    // if we got to this point, we know the input is good
+    break
+    // break quits the while loop
+    
+    }
 
